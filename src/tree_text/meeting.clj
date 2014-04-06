b67cfc998aad1d83924deb73766c96a38be6f0fa
Jukka Villstedt <jukka.villstedt@gmail.com>
2014-04-06 22:52:18 +0300

    x

--- src/tree_text/meeting.clj
+++ src/tree_text/meeting.clj
@@ -11,13 +11,12 @@
                      [:head
                       [:meta {:charset "utf-8"}]
                       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-                      [:link {:href "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" :rel "stylesheet"}]
-                      [:style (slurp "resources/meeting.css")]
-                      [:link {:href "css/meeting.css" :rel "stylesheet"}]]
+                      [:link {:href "https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" :rel "stylesheet"}]
+                      [:style (slurp "resources/meeting.css")]]
                      [:body [:div {:class "container"}
                              (hiccup/html contents)]
                       [:script {:src "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"}]
-                      [:script {:src "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"}]]])))
+                      [:script {:src "https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"}]]])))
 
 (defn words-to-string [words]
   (apply str (interpose " " words)))
@@ -54,12 +53,36 @@
 
       [(words-to-string all-children)])))
 
+(defn exists-in-tree? [tree predicate]
+  (and (vector? tree)
+       (or (predicate tree)
+           (some #(exists-in-tree? % predicate)  (drop 2 tree)))))
+
+(deftest contains-action-point?-test
+  (is (= (exists-in-tree? [:tree ":ap" "foo"] #(= (second %) ":ap"))
+         true))
+  (is (= (exists-in-tree? [:tree ":foo" [:tree ":apd" "foo"]] #(= (second %) ":ap"))
+         nil)))
+
+(defn prune-tree [tree predicate]
+  (into [:tree (second tree)] (map #(prune-tree % predicate)
+                                   (filter #(exists-in-tree? % predicate)
+                                           (drop 2 tree)))))
+(deftest action-points-test
+  (is (= (prune-tree [:tree ":ap" "foo"] #(= (second %) ":ap"))
+         true))
+  (is (= (prune-tree [:tree ":apz" [:tree ":aap" [:tree ":ap" "foo"]] [:tree ":apz" "bar"]] #(= (second %) ":ap"))
+         true)))
+
 (defn generate-html [source target]
   (let [tree (tree-text/parse (slurp source))
+        #_tree #_(prune-tree tree (fn [[ _ type responsible & others]] (println type) (and (= type ":ap")
+                                                                                   true #_(= responsible ":jukka"))))
         body (first (tree-text/transform tree
                                          meeting-body-to-hiccup))]
     (spit target
           (page body))))
 
+(run-tests)
 (generate-html "/Users/jukka/Dropbox/Public/Vaalit 2014/eurovaalityöryhmä_2014_04_06/eurovaalityöryhmä_2014_04_06.ttxt"
-               "/Users/jukka/Dropbox/Public/Vaalit 2014/eurovaalityöryhmä_2014_04_06/index.html")
+               "/Users/jukka/Dropbox/Public/Vaalit 2014/eurovaalityöryhmä_2014_04_06/index2.html")
